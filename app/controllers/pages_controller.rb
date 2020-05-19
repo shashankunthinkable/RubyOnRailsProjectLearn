681b5745622b710002cf79eac4b9773b50ae156e
shikshasamadhan <guptamrshashank@gmail.com>
2020-05-20 02:35:31 +0530

    about Route Introduce and Scaffold done with migration

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
   def Home
+=begin
+    render html: '"/" route => Home Function Executed'
+=end
+  end
 
+  def About
+    render html: '"/about" route => About Function Executed'
   end
 end
